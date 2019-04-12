218d67ea216d8fe7acbebcc0a384447ae8f9f219
Jan Michael Tangonan <janmichael.tangonan.2022@student.lhschools.org>
2019-04-12 14:00:04 -0400

    Third method reconfigured code

---
+++ src/MonthAndDayPredictionProject.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class MonthAndDayPredictionProject {
+
+public static void main(String[] args){
+
+
+}
+
+public static int writtenFormOfBirthMonth(){
+Scanner userBirthDayInput = new Scanner (System.in);
+
+
+}
+
+public static int dayOfTheWeek(){
+String h;
+int day,day2,day3,day4,day5,day6,day7;
+
+day = 1;
+day2 = 2;
+day3 = 3;
+day4 = 4;
+day5 = 5;
+day6 = 6;
+day7 = 7;
+
+
+return;}
+
+public static int poemDependingOnDay(){
+
+
+}
+
+}
---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,16 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+        System.out.print("My name is ");
+        System.out.println("Jan Michael");
+        System.out.println("and last summer");
+        System.out.println("My family went fishing");
+        System.out.println("It was a very fun experience");
+
+        // Make 3 more lines about what you did last summer (Completed)
+    }
+}
