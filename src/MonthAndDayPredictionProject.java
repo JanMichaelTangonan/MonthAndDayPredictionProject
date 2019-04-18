fe8a6fd0486111c6f168f71744713731c0618d6a
Jan Michael Tangonan <janmichael.tangonan.2022@student.lhschools.org>
2019-04-18 14:17:58 -0400

    Added months and switch statements

--- src/MonthAndDayPredictionProject.java
+++ src/MonthAndDayPredictionProject.java
@@ -2,17 +2,8 @@ import java.util.Scanner;
 
 public class MonthAndDayPredictionProject {
 
+
     public static void main(String[] args){
-        //Placeholder stuff
-        int a;
-        writtenFormOfBirthMonth(a);
-
-
-
-    }
-
-
-    public static void writtenFormOfBirthMonth(){
         //Variables
         int monthInput;
         int dayInput;
@@ -29,56 +20,152 @@ public class MonthAndDayPredictionProject {
 
         System.out.println("What year were you born?");
         yearInput = userBirthDayInput.nextInt();
+
+
+
+        //Placeholder stuff
+        int a;
+        monthsCalculation(a);
+
+        Scanner repeat = new Scanner(System.in);
+        int yes ;
+        int no;
+        if(repeat==yes){}
+
+
+    }
+
+
+    public static void monthsCalculation(){
+int m = 12;
+int q;
+String monthString;
+
+//Switch statements
+switch (m){
+    case 1:
+    monthString = "January";
+    break;
+
+    case 2:
+    monthString = "February";
+    break;
+
+    case 3:
+    monthString = "March";
+    break;
+
+    case 4:
+    monthString = "April";
+    break;
+
+    case 5:
+    monthString = "May";
+    break;
+
+    case 6:
+    monthString = "June";
+    break;
+
+    case 7:
+    monthString = "July";
+
+    case 8:
+    monthString = "August";
+    break;
+
+    case 9 :
+    monthString = "September";
+    break;
+
+    case 10:
+    monthString = "October";
+    break;
+
+    case 11:
+    monthString = "November";
+    break;
+
+    case 12:
+    monthString = "December";
+    break;
+}
+
+
 return;
     }
 
 
     public static int dayOfTheWeek(){
         //The days of the week
-        int h;
-        int dayOfTheWeek = h;
-        h saturday,sunday,monday,tuesday,wednesday,thursday,friday;
+        int h = 7;
+        String dayString;
 
-        0 = saturday;
-        1 = sunday;
-        2 = monday;
-        3 = tuesday;
-        4 = wednesday;
-        5 = thursday;
-        6 = friday;
+        switch (h) {
+            case 0:
+                dayString = "Saturday";
+                break;
 
+            case 1:
+                dayString = "Monday";
+                break;
 
+            case 2:
+                dayString = "Tuesday";
+
+            case 3:
+                dayString = "Wednesday";
+                break;
+
+            case 4:
+                dayString = "Thursday";
+                break;
+
+            case 5:
+                dayString = "Friday";
+                break;
+
+            case 6:
+                dayString = "Saturday";
+                break;
+
+            default:
+                dayString = "Invalid day";
+
+        }
+
+        return;
     }
 
     public static int poemDependingOnDay(){
         //Poems according to date of birth
 
-        if(writtenFormOfBirthMonth();){
+        if(monthsCalculation();){
             System.out.println("Monday's child is fair of face.");}
 
-        else if (writtenFormOfBirthMonth();){
+        else if (monthsCalculation();dayOfTheWeek()){
             System.out.println("Tuesday's child is full of grace.");
 
         }
 
-        else if (writtenFormOfBirthMonth();){
+        else if (monthsCalculation();dayOfTheWeek()){
             System.out.println("Wednesday's child is full of woe.");
 
         }
 
-        else if (writtenFormOfBirthMonth();){
+        else if (monthsCalculation();dayOfTheWeek()){
             System.out.println("Thursday's child has far to go.");
         }
 
-        else if(writtenFormOfBirthMonth();){
+        else if(monthsCalculation();dayOfTheWeek()){
             System.out.println("Friday's child loving and giving.");
         }
 
-        else if(writtenFormOfBirthMonth();){
+        else if(monthsCalculation();dayOfTheWeek()){
             System.out.println("Saturday's child works hard for a living");
         }
 
-        else if(writtenFormOfBirthMonth();){
+        else if(monthsCalculation();dayOfTheWeek()){
             System.out.println("But the child born on Sabbath day, is fair and wise and good in every way.");
         }
     }
